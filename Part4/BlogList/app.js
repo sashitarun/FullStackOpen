f25bf23ee3a7932a830067ca2c23c650a86be4ed
Sashi Tarun <sashitarunk@gmail.com>
2020-06-03 19:12:32 +0530

    Creating part4

---
+++ Part4/BlogList/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+logger.info(`Connecting to port ${config.PORT}`)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
