3c402e56a8746a5132213f54319fb625fe3c4a37
Sashi Tarun <sashitarunk@gmail.com>
2020-06-10 14:06:10 +0530

    adding JWT auth

--- Part4/BlogList/app.js
+++ Part4/BlogList/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 logger.info(`Connecting to port ${config.PORT}`)
@@ -26,7 +27,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
-app.use('/api/users',usersRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- Part4/BlogList/controllers/blogs.js
+++ Part4/BlogList/controllers/blogs.js
@@ -1,7 +1,18 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog.js')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => 
+{
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer '))
+    {
+        return authorization.substring(7)
+    }
+    return null
+} 
 
 blogsRouter.get('/',async (request, response) => {
     
@@ -9,10 +20,20 @@ blogsRouter.get('/',async (request, response) => {
     response.json(blogs.map((b) => b.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response,next) => {
 
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token,process.env.SECRET)
+    if(!token || !decodedToken.id)
+    {
+        response.status(401).json(
+            {
+                error : "Invalid or Missing token"
+            }
+        )
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title : body.title,
@@ -33,7 +54,7 @@ blogsRouter.post('/', async (request, response) => {
         }
         await blog.save()
         user.blogs = user.blogs.concat(blog._id)
-        await user.save()
+        await user.save().catch(error => next(error))
         response.json(blog.toJSON())
     }
 })
