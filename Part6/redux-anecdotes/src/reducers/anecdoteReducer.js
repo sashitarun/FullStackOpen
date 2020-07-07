e10d6ce1da41dfab691df818804d0a70ee388535
Sashi Tarun <sashitarunk@gmail.com>
2020-07-07 12:14:44 +0530

    till 6.13

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,41 +1,5 @@
-const anecdotesAtStart = [
-  { 
-    content : 'If it hurts, do it more often',
-    votes : 0
-  },
-  { 
-    content : 'Adding manpower to a late software project makes it later!',
-    votes : 0
-  },
-  {
-    content : 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    votes : 0
-  },
-  { 
-    content : 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    votes : 0
-  },
-  { 
-    content :'Premature optimization is the root of all evil.',
-    votes : 0
-  },
-  { 
-    content : 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    votes : 0
-  }
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote.content,
-    id: getId(),
-    votes: anecdote.votes
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 export const addVote = (id) =>
 {
@@ -54,11 +18,21 @@ export const addNewAnecdote = (content) =>
   })
 }
 
-const anecdoteReducer = (state = initialState, action) => {
-  // console.log('state now: ', state)
-  // console.log('action', action)
+export const initialiseAnecdotes = (anecdotes) =>
+{
+  return({
+    type : 'INIT_ANECDOTES',
+    data : {
+      anecdotes : anecdotes
+    }
+  })
+}
 
+const anecdoteReducer = (state = [], action) => {
+ 
   switch(action.type){
+    case 'INIT_ANECDOTES':
+      return action.data.anecdotes
     case 'ADD_VOTE':
       const anecdote_id = action.data.id
       const requiredAnecdote = state.find(anecdote => anecdote.id === anecdote_id)
